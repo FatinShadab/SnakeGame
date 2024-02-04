a6e24c07054d58e7954a0822b64aa0ead06487c8
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-02-04 18:12:09 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -60,14 +60,31 @@ class Snake:
     def grow(self):
         self.body.append(self.body[-1] + self.direction)
 
+    def reset(self):
+        self.direction = self.defaultDirection
+        
+        self.body = [
+            pygame.math.Vector2(6, 9),
+            pygame.math.Vector2(5, 9),
+            pygame.math.Vector2(4, 9)
+        ]
+        
+        self.head = self.body[0]
+        
 
 class Game:
+    Menu = 0
+    Play = 2
+    Pause = 3
+    
     AUTO_SNAKE_MOVEMENT = pygame.event.custom_type()
-    #AUTO_FOOD_GENERATION = pygame.event.custom_type()
+    AUTO_HISS_SOUND = pygame.event.custom_type()
     
     def __init__(self):
         pygame.init()
+        self.state = Game.Menu
         self.scoreValue = 0
+        self.highScoreValue = 0
         self.cellCount = 16
         self.cellSize = 50
         self.windowWH = (
@@ -78,7 +95,8 @@ class Game:
         self.runFlag = True
         self.window = pygame.display.set_mode(self.windowWH)
         self.gameClock = pygame.time.Clock()
-        self.font = pygame.font.Font("resources/Pixeltype.ttf", 60)
+        self.font = pygame.font.Font("resources/Pixeltype.ttf", 50)
+        self.font.bold = True
         self.maxFPS = 60
         self.backgroundColor = (173, 204, 96)
         pygame.display.set_caption("PySnake")
@@ -86,19 +104,30 @@ class Game:
         self.snake = Snake((self.cellSize + 10, self.cellSize + 10))
         
         self.food = Food((2*self.cellSize, 2*self.cellSize))
-        self.foodPos = self.foodPos = self.food.get_random_render_pos(
-            (50,  (self.cellCount - 2) * self.cellSize),
-            (50,  (self.cellCount - 2) * self.cellSize)
-        )
+        self.foodPos = None
         
-        self.recordedUserEvent = None
+        self.playArea = pygame.Rect(5, 40, self.width - 10, self.height - 45)
         
         pygame.time.set_timer(Game.AUTO_SNAKE_MOVEMENT, 200)
-        #pygame.time.set_timer(Game.AUTO_FOOD_GENERATION, 5000)
+        pygame.time.set_timer(Game.AUTO_HISS_SOUND, 5000)
+        
+        self.config_audio()
+        
+    def config_audio(self):
+        self.eatSE = pygame.mixer.Sound("resources/hiss3-103123.mp3")
+        self.hitSE = pygame.mixer.Sound("resources/loss.mp3")
+        self.hissSE = pygame.mixer.Sound("resources/hiss.mp3")
+        
+        self.eatSE.set_volume(0.5)
+        self.hitSE.set_volume(0.5)
+        self.hissSE.set_volume(0.5)
+
+    def update_high_sore(self):
+        self.highScoreValue = max(self.highScoreValue, self.scoreValue)
 
     def handle_food_snake_collision(self):
         food_solid_area = self.foodPos.copy()
-        food_solid_area.width = 35
+        food_solid_area.width = 40
         food_solid_area.height = 40
         food_solid_area.x += 35
         food_solid_area.y += 25
@@ -112,57 +141,100 @@ class Game:
             )
             self.snake.grow()
             self.scoreValue += 1
+            self.eatSE.play()
+
+    def termination_event_ocurred(self):
+        snakeHead = self.snake.get_snake_head()
+        for bodyPart in self.snake.get_render_object()[1:]:
+            if snakeHead.contains(bodyPart):
+                return True
+        return not self.playArea.contains(snakeHead)
+
+    def render_menu(self):
+        pygame.draw.circle(self.window, "Black", (self.width/2, self.height/2), self.width / 2.4)
+        pygame.draw.circle(self.window, (30, 60, 30), (self.width/2, self.height/2), self.width / 2.5)
+        
+        if self.termination_event_ocurred():
+            self.window.blit(self.font.render(f"Score : {self.scoreValue}",False, (6, 2, 26)), ((self.width / 4) + 120, self.height / 2.6))
+            self.window.blit(self.font.render(f"High Score : {self.highScoreValue}",False, (6, 2, 26)), ((self.width / 4) + 100, self.height / 2.2))
+            self.window.blit(self.font.render(f"Press Enter To Play !", False, (6, 2, 26)), ((self.width / 4) + 50, self.height / 1.8))
+        else:
+            self.window.blit(self.font.render(f"Use Arrow keys to Move !", False, (6, 2, 26)), ((self.width / 5) + 70, self.height / 2.6))
+            self.window.blit(self.font.render(f"Space key to Pause !", False, (6, 2, 26)), ((self.width / 4) + 60, self.height / 2.2))
+            self.window.blit(self.font.render(f"Press Enter To Play !", False, (6, 2, 26)), ((self.width / 4) + 55, self.height / 1.8))        
 
     def render(self):
         self.window.fill(self.backgroundColor)
         
-        self.window.blit(self.font.render(f"Score {self.scoreValue}", False, "White"), (5, 5))
+        pygame.draw.rect(self.window, (6, 2, 26), self.playArea, 15)
         
-        self.window.blit(
-            self.food.get_render_object(),
-            self.foodPos
-        )
+        self.window.blit(self.font.render(f"Score {self.scoreValue}", False, (6, 2, 26)), (10, 5))
+        self.window.blit(self.font.render(f"High Score : {self.highScoreValue}",False, (6, 2, 26)), ((self.width - 400) + 100, 5))
+        
+        if self.foodPos:
+            self.window.blit(
+                self.food.get_render_object(),
+                self.foodPos
+            )
         
         for idx, renderObj in enumerate(self.snake.get_render_object()):
             if idx == 0:
-                pygame.draw.rect(self.window, Snake.COLOR, renderObj, 0, 20)
+                pygame.draw.rect(self.window, Snake.COLOR, renderObj, 0, 26)
             else:
                 pygame.draw.rect(self.window, Snake.COLOR, renderObj, 0, 16)
+
+        if self.state == Game.Menu:
+            self.render_menu()
+            
+        if self.state == Game.Pause:
+            pygame.draw.rect(self.window, Snake.COLOR, pygame.Rect(200, 5, 10, 20))
+            pygame.draw.rect(self.window, Snake.COLOR, pygame.Rect(215, 5, 10, 20))
 
     def input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.runFlag = False
-                
-            self.recordedUserEvent = event
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN and self.state == Game.Menu:
+                    self.scoreValue = 0
+                    self.snake.reset()
+                    self.foodPos = self.food.get_random_render_pos(
+                            (50,  (self.cellCount - 2) * self.cellSize),
+                            (50,  (self.cellCount - 2) * self.cellSize)
+                    )
+                    self.state = Game.Play
+                    
+                if event.key == pygame.K_SPACE:
+                    if self.state == Game.Play:
+                        self.state = Game.Pause
+                    elif self.state == Game.Pause:
+                        self.state = Game.Play
+                elif event.key == pygame.K_UP:
+                    self.snake.direction = Direction.UP
+                elif event.key == pygame.K_DOWN:
+                    self.snake.direction = Direction.DOWN
+                elif event.key == pygame.K_RIGHT:
+                    self.snake.direction = Direction.RIGHT
+                elif event.key == pygame.K_LEFT:
+                    self.snake.direction = Direction.LEFT
+                    
+            elif event.type == Game.AUTO_SNAKE_MOVEMENT:
+                if self.state == Game.Play:
+                    self.snake.update()
+            
+            elif event.type == Game.AUTO_HISS_SOUND:
+                if self.state == Game.Play:
+                    self.hissSE.play()
+                    
 
     def update(self):
-        if self.recordedUserEvent:
-            if self.recordedUserEvent.type == Game.AUTO_SNAKE_MOVEMENT:
-                self.snake.update()
-            
-            # if self.recordedUserEvent.type == Game.AUTO_FOOD_GENERATION:
-            #     self.foodPos = self.food.get_random_render_pos(
-            #         (50,  (self.cellCount - 2) * self.cellSize),
-            #         (50,  (self.cellCount - 2) * self.cellSize)
-            #     )
-                
-            if self.recordedUserEvent.type == pygame.KEYDOWN:
-                if self.recordedUserEvent.key == pygame.K_UP:
-                    self.snake.direction = Direction.UP
-                if self.recordedUserEvent.key == pygame.K_DOWN:
-                    self.snake.direction = Direction.DOWN
-                if self.recordedUserEvent.key == pygame.K_RIGHT:
-                    self.snake.direction = Direction.RIGHT
-                if self.recordedUserEvent.key == pygame.K_LEFT:
-                    self.snake.direction = Direction.LEFT
-            
-            self.recordedUserEvent = None
-            
+        self.update_high_sore()
         self.handle_food_snake_collision()
-
-        pygame.display.flip()
-        pygame.display.update()
+        
+        if self.termination_event_ocurred():
+            self.hitSE.play()
+            self.state = Game.Menu
 
     def cleanup(self):
         pygame.quit()
@@ -172,10 +244,15 @@ class Game:
         while self.runFlag:
             
             self.input()
-            self.update()
+            
+            if self.state == Game.Play:
+                self.update()
+            
             self.render()
 
             self.gameClock.tick(self.maxFPS)
+            pygame.display.flip()
+            pygame.display.update()
 
     def run(self):
         self.gameLoop()
